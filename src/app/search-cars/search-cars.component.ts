4779927279c870af81ebc1804ec01dcfa2d3b0f7
Hristo Papucharov <hpapucharov@gmail.com>
2021-05-18 10:20:07 -0500

    API Call

--- src/app/search-cars/search-cars.component.ts
+++ src/app/search-cars/search-cars.component.ts
@@ -7,14 +7,12 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./search-cars.component.css']
 })
 export class SearchCarsComponent implements OnInit {
+  cars: any;
 
   constructor(private http: HttpClient) { }
   ngOnInit(): void {
     this.http
-      .get(`https://radiant-sierra-38985.herokuapp.com/api/inventory`)
-      .subscribe(response => {
-        console.log(response);
+      .get(`https://radiant-sierra-38985.herokuapp.com/api/cars`)
+      .subscribe(response => this.cars = response);
       });
   }
-
-}
