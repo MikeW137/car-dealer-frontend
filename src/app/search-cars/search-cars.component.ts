f3e44111c1bd6946ad279ddfcddb52d1230e0fa7
Hristo Papucharov <hpapucharov@gmail.com>
2021-05-19 12:18:12 -0500

    CSS Flexbox update

--- src/app/search-cars/search-cars.component.ts
+++ src/app/search-cars/search-cars.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-search-cars',
