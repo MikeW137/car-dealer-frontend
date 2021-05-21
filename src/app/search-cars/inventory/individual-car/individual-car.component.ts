4786e68b382ca573647d6548fa80b0e8768bdc3e
Michael Wydra <wydrama@gmail.com>
2021-05-21 10:27:48 -0500

    added for loop images on inventory page

--- src/app/search-cars/inventory/individual-car/individual-car.component.ts
+++ src/app/search-cars/inventory/individual-car/individual-car.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import { ActivatedRoute} from '@angular/router';
 import { HttpClient} from '@angular/common/http';
 import { UrlSet } from '../../../../url';
@@ -8,10 +8,10 @@ import { UrlSet } from '../../../../url';
   templateUrl: './individual-car.component.html',
   styleUrls: ['./individual-car.component.css']
 })
-export class IndividualCarComponent implements OnInit {
+export class IndividualCarComponent implements OnInit, AfterViewInit {
   inventory: any;
   url = UrlSet.url;
-
+  imageList: any;
   constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
@@ -20,8 +20,16 @@ export class IndividualCarComponent implements OnInit {
         let id = parseInt(params.get('id'));
         this.http
           .get(`${this.url}/inventory/${id}`)
-          .subscribe(response => this.inventory = response);
+          .subscribe(response => {
+            this.inventory = response;
+            this.imageList = this.inventory.imageList;
+          });
       });
+  }
+
+  ngAfterViewInit(){
+    let test = document.getElementById('0');
+    test.classList.add("active");
   }
 
 }
